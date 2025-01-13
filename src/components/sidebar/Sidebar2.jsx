f634135c279b112badc4949626002b97a9ac133c
jhnl18 <jhunelpenaflorida@gmail.com>
2025-01-14 01:35:48 +0800

    Revamped the Sidebar

---
+++ src/components/sidebar/Sidebar2.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Dropdown, NavLink } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOut } from "@fortawesome/free-solid-svg-icons";
+import "./sidebar.css";
+import UserProfile from "../../ui/UserProfile";
+import { AuthenticationService } from "../../services/AuthenticationService";
+
+import image from "../../assets/avatar.png";
+
+const Sidebar = ({ menuItems, toggleSidebar }) => {
+  const [isNotActive, setNotActive] = useState(false);
+  const [expandedIndex, setExpandedIndex] = useState(null);
+  
+  const navigate = useNavigate();
+  const handleLogout = () => {
+      AuthenticationService.logout();
+      navigate("/"); // Redirect to the login page after logout
+    };
+
+  const handleClick = (item, index, event) => {
+    if (item.subMenu) {
+      event.preventDefault();
+      setExpandedIndex(expandedIndex === index ? null : index);
+    }
+  };
+
+  return (
+    <div>
+      <div className="wrapper">
+        <nav id="sidebar" className={isNotActive ? "active" : ""}>
+          <button
+            type="button"
+            id="sidebarCollapse"
+            onClick={() => setNotActive(!isNotActive)}
+            className="btn btn-custom"
+          >
+            {isNotActive ? (
+              <i className="fas fa-bars"></i>
+            ) : (
+              <i className="fas fa-times-circle"></i>
+            )}
+          </button>
+          <div className="sidebar-header">
+            <img
+              src={image}
+              className="rounded-circle usr-image"
+              height={isNotActive ? "20" : "70"}
+              width={isNotActive ? "20" : "70"}
+              alt="User Avatar"
+            />
+            <h3>
+              <UserProfile />
+            </h3>
+          </div>
+
+          <ul className="list-unstyled components">
+            {menuItems.map((item, index) => (
+              <li key={index} className="list-item">
+                <FontAwesomeIcon icon={item.icon} className="icon-color" />
+                <Link
+                  to={item.subMenu ? "#" : item.to}
+                  className={item.subMenu ? "dropdown-toggle" : ""}
+                  onClick={(event) => handleClick(item, index, event)}
+                >
+                  {item.name}
+                </Link>
+                {item.subMenu && expandedIndex === index && (
+                  <ul className="list-unstyled ms-1">
+                    {item.subMenu.map((subItem, subIndex) => (
+                      <li key={subIndex} className="sidebar-item">
+                        <Link to={subItem.to}>{subItem.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/layouts/root-layout/RootLayout.jsx
+++ src/layouts/root-layout/RootLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Header from '../../ui/Header';
-import Sidebar from '../../components/sidebar/Sidebar';
+import Sidebar from '../../components/sidebar/Sidebar2';
 
 const RootLayout = ({ children, menuItems, username }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -12,28 +12,24 @@ const RootLayout = ({ children, menuItems, username }) => {
 
   return (
     <div>
-      
       <Container fluid>
-        
         <Row>
-          <Sidebar 
-            menuItems={menuItems} 
-            username={username} 
-            isSidebarOpen={isSidebarOpen} 
-            toggleSidebar={toggleSidebar} 
+          <Sidebar
+            menuItems={menuItems}
+            username={username}
+            isSidebarOpen={isSidebarOpen}
+            toggleSidebar={toggleSidebar}
           />
-          <Col 
-            className="px-0" 
-            style={{ 
-              marginLeft: isSidebarOpen ? '250px' : '0',  
-              transition: 'margin-left 0.2s ease-in-out', 
-              width: isSidebarOpen ? 'calc(100% - 250px)' : '100%',
-              marginTop:'56px'
-              // paddingLeft: isSidebarOpen ? '20px' : '0',
-              // paddingRight: isSidebarOpen ? '20px' : '0',
+          <Col
+            className="px-0"
+            style={{
+              transition: 'margin-left 0.3s ease-in-out',
+              marginLeft: isSidebarOpen ? '250px' : '50px',  // Sidebar width adjusted here
+              width: isSidebarOpen ? 'calc(100% - 250px)' : '100%',  // Adjust content width based on sidebar state
+              marginTop: '56px', // Adjust to prevent header overlap
             }}
           >
-            <Header username={username} toggleSidebar={toggleSidebar} />
+            {/* <Header username={username} toggleSidebar={toggleSidebar} /> */}
             {children}
           </Col>
         </Row>
